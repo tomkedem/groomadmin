fbb85a45705e4f03d130aa347b701921145cc65c
Tomer Kedem <tompha1@hotmail.com>
2023-12-16 14:16:12 +0200

    First init

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using Grpc.Net.Client;
+using gRoom.gRPC.Messages;
+using Google.Protobuf.WellKnownTypes;
+
+using var channel = GrpcChannel.ForAddress("http://localhost:5054");
+var client = new Groom.GroomClient(channel);
+
+Console.WriteLine("*** Admin Console started ***");
+Console.WriteLine("Listening...");
+
+// ADD THE MONITORING CODE BELOW THIS LINE
